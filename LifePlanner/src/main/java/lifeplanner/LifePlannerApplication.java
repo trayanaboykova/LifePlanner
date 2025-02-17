4f1c65c5b73d68f74cd11511b7ed23c13f3739e8
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-17 18:56:40 +0200

    adding initial resources

---
+++ LifePlanner/src/main/java/lifeplanner/LifePlannerApplication.java
@@ -0,0 +1,13 @@
+package lifeplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifePlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifePlannerApplication.class, args);
+	}
+
+}
