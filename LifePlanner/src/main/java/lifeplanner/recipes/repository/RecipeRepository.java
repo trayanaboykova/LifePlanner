fa8dff1948672c9c4abb7e624e00f5bb5ee6797e
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-27 17:04:39 +0200

    Create RecipeRepository.java

---
+++ LifePlanner/src/main/java/lifeplanner/recipes/repository/RecipeRepository.java
@@ -0,0 +1,12 @@
+package lifeplanner.recipes.repository;
+
+import lifeplanner.recipes.model.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RecipeRepository extends JpaRepository<Recipe, UUID> {
+
+}
