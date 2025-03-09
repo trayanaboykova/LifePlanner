925b59e7c72be104bddb4499b9569be96c21bda8
Trayana Boykova <trayana.boykova@gmail.com>
2025-03-09 09:06:25 +0200

    Create SessionCheckInterceptor.java

---
+++ LifePlanner/src/main/java/lifeplanner/security/SessionCheckInterceptor.java
@@ -0,0 +1,17 @@
+package lifeplanner.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class SessionCheckInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+}
