e7fa4fe282ba7a6fd259edd9d174449275c36c19
Trayana Boykova <trayana.boykova@gmail.com>
2025-03-10 19:25:25 +0200

    Create WebMvcConfiguration.java

---
+++ LifePlanner/src/main/java/lifeplanner/config/WebMvcConfiguration.java
@@ -0,0 +1,21 @@
+package lifeplanner.config;
+
+import lifeplanner.security.SessionCheckInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    private SessionCheckInterceptor interceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/images/**", "/js/**");
+    }
+}
