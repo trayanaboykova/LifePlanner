59e8726b7e64008c9b9e39a7a41c63d3d636f774
Trayana Boykova <trayana.boykova@gmail.com>
2025-03-26 11:16:04 +0200

    Update BookAlreadySharedException.java

--- LifePlanner/src/main/java/lifeplanner/exception/books/BookAlreadySharedException.java
+++ LifePlanner/src/main/java/lifeplanner/exception/books/BookAlreadySharedException.java
@@ -8,7 +8,6 @@ import java.util.UUID;
 @Getter
 public class BookAlreadySharedException extends DomainException {
     private final UUID bookId;
-
     public BookAlreadySharedException(UUID bookId) {
         super("Book " + bookId + " is already shared");
         this.bookId = bookId;
